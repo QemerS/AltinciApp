8dbe4c0b2b91a0d0a329cbfe29dfc5b8a67ac98d
QemerS <tu201806169@code.edu.az>
2021-10-26 06:33:43 +0400

    Add project files.

---
+++ AltinciApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AltinciApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Write a c# program that takes the radius of a circle as input
+             * and calculate perimeter (l=2*pi*r)
+             * and area (S=pi*r^2) of the circle
+            */
+            Console.WriteLine("Enter the radius of a circle");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            double l = 2 * Math.PI * r;
+            double S = Math.PI * r * r;
+
+            Console.WriteLine("Perimeter of circle is {0:0.00}", l);
+            Console.WriteLine("Area of circle is {0:0.00}", S);
+        }
+    }
+}
